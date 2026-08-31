6faa9a6a6e2da4e0844f8f22f033c1a33871258d
JhairEspindola <nikitabingobango@gmail.com>
2026-08-31 08:36:27 -0500

    añado carpeta

---
+++ inventario_front/src/components/text.jsx
@@ -0,0 +1,11 @@
+import  "./text.css";
+
+function Text() {
+  return (
+    <>
+      <h1 className="titulo">Bienvenido al sistema de Inventario</h1>
+      <h1 className="subtitulo">¡Jhair Espindola buscando a Nikita!</h1>
+    </>
+  );
+}
+export default Text;
---
+++ inventario_front/src/pages/welcome.jsx
@@ -0,0 +1,13 @@
+import Text from "../components/text";
+import "./welcome.css";
+
+function Welcome() {
+  return (
+
+    <div className="welcome-container">
+    <Text/>
+    </div>
+    
+  );
+}
+export default Welcome;
